a6927ef7b97212cb9f183e1ebf5a58cb5522c24e
Robert Egeland <robegeland@yahoo.com>
2022-01-04 22:43:46 -0600

    commit tree a827fd387b13fe486d0381abcbf87e55cff922c1

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+
+
+function submitData(name, email) {
+    const userObj = {
+        'name': name,
+        'email': email,
+    }
+   return fetch('http://localhost:3000/users', {
+       method: 'POST',
+       headers: {
+           'Content-Type': 'application/json',
+           Accept: 'application/json'
+       },
+       body: JSON.stringify(userObj)
+   })
+   .then(function (response) {
+       return response.json();
+   })
+   .then(function (object) {
+       addID(object.id)
+   })
+   .catch(function (error) {
+       addError(error.message);
+   })
+}
+function addID(newID) {
+    document.getElementsByTagName('body')[0].innerHTML = newID
+}
+function addError(error) {
+    document.getElementsByTagName('body')[0].innerHTML = error
+}
+submitData()
+
+
